d6c0a9240e2c7715256e0143be712a07fb4d357b
James Dabbs <jamesdabbs@gmail.com>
2016-07-11 10:25:55 -0400

    Extract common test setup

--- app/models/strategies/log.rb
+++ app/models/strategies/log.rb
@@ -2,11 +2,13 @@ module Strategies
   class Log
     def initialize path
       @path = path
+      @file = File.open path, "w"
     end
 
     def call msg
       ts = msg.timestamp.strftime '%Y/%m/%d %H:%M:%S'
-      File.write @path, "[#{ts}] #{msg.user} - #{msg.text}"
+      @file.puts "[#{ts}] #{msg.user} - #{msg.text}"
+      @file.flush
     end
   end
 end
--- spec/models/strategies/log_spec.rb
+++ spec/models/strategies/log_spec.rb
@@ -1,19 +1,28 @@
 require "rails_helper"
 
 describe Strategies::Log do
-  it "writes messages to a file" do
-    log_file = "./spec/files/log.txt"
-    time = Time.parse "2012-02-12 12:41:32"
-
-    s = Strategies::Log.new log_file
-    message = Message.new(
+  let(:log_file) { "./spec/files/log.txt" }
+  let(:time) { Time.parse "2012-02-12 12:41:32" }
+  let(:message) {
+    Message.new(
       text:     "hello",
       user:     "someone",
       timestamp: time
     )
+  }
 
+  it "writes messages to a file" do
+    s = Strategies::Log.new log_file
     s.call message
 
     expect(File.read log_file).to include "[2012/02/12 12:41:32] someone - hello"
+  end
+
+  it "appends to the log file" do
+    s = Strategies::Log.new log_file
+
+    3.times { s.call message }
+
+    expect(File.read(log_file).lines.count).to eq 3
   end
 end
