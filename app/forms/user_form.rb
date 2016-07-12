5d7875346a7bf0dba2478c1e7b4093292e5917ee
James Dabbs <jamesdabbs@gmail.com>
2016-07-12 09:47:26 -0400

    Working user form

--- app/forms/user_form.rb
+++ app/forms/user_form.rb
@@ -12,9 +12,9 @@ class UserForm
 
   def initialize id: nil
     if id
-      @user              = User.find opts[:id]
+      @user              = User.find id
       self.username      = @user.name
-      self.phone_numbers = @user.phones.pluck :number
+      self.phone_numbers = @user.phones.pluck(:number) + [""]
     else
       @user              = User.new
       self.username      = ""
@@ -37,14 +37,36 @@ class UserForm
   # end
 
   # More magic
-  0.upto(9) do |i|
-    define_method "phone_#{i}" do
-      phone_numbers[i]
+  # 0.upto(9) do |i|
+  #   define_method "phone_#{i}" do
+  #     phone_numbers[i]
+  #   end
+  # end
+
+  def method_missing name, *args
+    if name.to_s.start_with? "phone_"
+      number = name.to_s.sub("phone_", "").to_i
+      phone_numbers[number]
+    else
+      super name, *args
     end
   end
 
   def save updates
     # TODO: do updates
+    # username, phone_0, phone_1, ....
+    self.username = updates[:username]
+    updates.each do |key, value|
+      if key.to_s.start_with? "phone_"
+        number = key.to_s.sub("phone_", "").to_i
+        phone_numbers[number] = value
+      end
+    end
+
+    # Idea:
+    # updates.each do |k,v|
+    #   send "#{k}=", v
+    # end
 
     return false unless valid?
 
@@ -59,6 +81,14 @@ class UserForm
     phone_numbers.each do |n|
       @user.phones.where(number: n).first_or_create! if n.present?
     end
+  end
+
+  def persisted?
+    @user.persisted?
+  end
+
+  def id
+    @user.id
   end
 
   private
